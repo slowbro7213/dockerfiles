55c6394bca12c53475cdd7b4120777e199e52c5c
slowbro7213 <59325333+slowbro7213@users.noreply.github.com>
2023-02-22 21:18:04 +0900

    Create main.py

---
+++ python-cron/apscheduler/main.py
@@ -0,0 +1,53 @@
+import argparse
+import time
+
+import daemon
+from daemon import pidfile
+
+from src import logger
+from src import pid
+
+from src.common import fileutil
+
+
+ARG_DAEMON = None
+
+
+def set_argument():
+
+    parser = argparse.ArgumentParser(description='daemon/scheduler test module.', usage='python main.py <flags>')
+    parser.add_argument('--daemon', '-d', default='no', dest='daemon', help='run mode. default: no ( --daemon|-d no|start|stop )')
+    parser.add_argument('--debug', dest='debug', action="store_true", help='use log level to debug. default: False ( --debug )')
+    args = parser.parse_args()
+
+    if args.daemon.lower() != 'no' and args.daemon.lower() != 'start' and args.daemon.lower() != 'stop':
+        raise Exception('Error: invalid args. --daemon|-d: no, start or stop')
+
+    global ARG_DAEMON
+    ARG_DAEMON = args.daemon.lower()
+
+
+if __name__ == '__main__':
+
+    log = logger.get_logger()
+
+    set_argument()
+    pid.remove_old_pidfile()
+
+    if ARG_DAEMON == 'no':
+        print('no daemon')
+
+    elif ARG_DAEMON == 'start':
+        print('start daemon')
+        with daemon.DaemonContext(working_directory=fileutil.get_project_root(), umask=0o002, pidfile=pidfile.TimeoutPIDLockFile(pid.PID_PATH)) as cxt:
+            while(True):
+                time.sleep(10)
+
+    elif ARG_DAEMON == 'stop':
+        print('stop daemon')
+        pid.kill_process_if_exist()
+
+    else:
+        log.error(f'Error: invalid mode ({ARG_DAEMON})')
+        exit(1)
+    exit(0)
