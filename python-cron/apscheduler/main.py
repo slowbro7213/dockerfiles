5202e319d98579a12cdf62aa13a3393565e7fc2a
slowbro7213 <59325333+slowbro7213@users.noreply.github.com>
2023-02-28 18:19:44 +0900

    Update main.py to use BlockingScheduler

--- python-cron/apscheduler/main.py
+++ python-cron/apscheduler/main.py
@@ -1,32 +1,36 @@
 import argparse
 import time
 
-import daemon
-from daemon import pidfile
+#from apscheduler.schedulers.background import BackgroundScheduler
+from apscheduler.schedulers.blocking import BlockingScheduler
 
 from src import logger
 
-from src.common import fileutil
 
-
-ARG_DAEMON = None
 ARG_DEBUG = None
 
 
 def set_argument():
 
     parser = argparse.ArgumentParser(description='daemon/scheduler test module.', usage='python main.py <flags>')
-    parser.add_argument('--daemon', '-d', default='no', dest='daemon', help='run mode. default: no ( --daemon|-d no|start|stop )')
     parser.add_argument('--debug', dest='debug', action="store_true", help='use log level to debug. default: False ( --debug )')
     args = parser.parse_args()
 
-    if args.daemon.lower() != 'no' and args.daemon.lower() != 'start' and args.daemon.lower() != 'stop':
-        raise Exception('Error: invalid args. --daemon|-d: no, start or stop')
-
-    global ARG_DAEMON
     global ARG_DEBUG
-    ARG_DAEMON = args.daemon.lower()
     ARG_DEBUG = args.debug
+
+
+def job_test1(arg1):
+    log = logger.get_logger()
+    log.info(f'job start: {time.strftime(arg1)}')
+    time.sleep(10)
+    log.info(f'job end: {time.strftime(arg1)}')
+
+
+def run_scheduler():
+    scheduler = BlockingScheduler()
+    scheduler.add_job(job_test1, 'cron', second='*/5', id='job_1', args=['%H:%M:%S'])
+    scheduler.start()
 
 
 if __name__ == '__main__':
@@ -35,22 +39,6 @@ if __name__ == '__main__':
     set_argument()
     if ARG_DEBUG:
         log = logger.set_level_to_debug()
-    pid.remove_old_pidfile()
 
-    if ARG_DAEMON == 'no':
-        print('no daemon')
-
-    elif ARG_DAEMON == 'start':
-        print('start daemon')
-        with daemon.DaemonContext(working_directory=fileutil.get_project_root(), umask=0o002, pidfile=pidfile.TimeoutPIDLockFile(pid.PID_PATH)) as cxt:
-            while(True):
-                time.sleep(10)
-
-    elif ARG_DAEMON == 'stop':
-        print('stop daemon')
-        pid.kill_process_if_exist()
-
-    else:
-        log.error(f'Error: invalid mode ({ARG_DAEMON})')
-        exit(1)
+    run_scheduler()
     exit(0)
